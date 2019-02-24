8b324e942d66ed2ab703d00440ab2a0cfce4df68
zulune <mars.xaker@gmail.com>
2019-02-24 14:04:06 +0200

    Render post in index page

---
+++ src/posts/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+# Create your models here.
+
+
+User = get_user_model()
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, verbose_name=_(
+        "Author"), on_delete=models.CASCADE)
+    profile_picture = models.ImageField(_("Profile picture"))
+
+    def __str__(self):
+        return self.user.username
+
+
+class Category(models.Model):
+    title = models.CharField(_("Title"), max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(_("Title"), max_length=50)
+    overview = models.TextField(_("Overview"))
+    timestamp = models.DateTimeField(
+        _("Timestamp"), auto_now=False, auto_now_add=True)
+    comment_count = models.IntegerField(_("Comment count"), default=0)
+    author = models.ForeignKey(Author, verbose_name=_(
+        "Author"), on_delete=models.CASCADE)
+    thumbnail = models.ImageField(_("Thumbnail"))
+    categories = models.ManyToManyField(Category, verbose_name=_("Categories"))
+    featured = models.BooleanField(_("Featured"), default=False)
+
+    def __str__(self):
+        return self.title
---
+++ src/posts/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from .models import Post
+# Create your views here.
+
+
+def index(request):
+    featured = Post.objects.filter(featured=True)
+    latest = Post.objects.order_by('-timestamp')[0:3]
+    context = {
+        'object_list': featured,
+        'latest': latest
+    }
+    return render(request, "index.html", context)
+
+
+def blog(request):
+    return render(request, "blog.html", {})
+
+
+def post(request):
+    return render(request, "post.html", {})
