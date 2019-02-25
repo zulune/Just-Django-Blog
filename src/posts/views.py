726a0d44478ad01a4af4b40a17999b4fc0fc83e6
zulune <mars.xaker@gmail.com>
2019-02-25 22:08:51 +0200

    Category count in post view

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,9 +1,19 @@
+from django.db.models import Count
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 from marketing.models import SignUp
 # Create your views here.
+
+
+def get_category_count():
+    queryset = Post \
+        .objects \
+        .values('categories__title') \
+        .annotate(Count('categories__title'))
+    return queryset
+
 
 
 def index(request):
@@ -24,6 +34,7 @@ def index(request):
 
 
 def blog(request):
+    category_count = get_category_count()
     most_recent = Post.objects.order_by("-timestamp")[:3]
     post_list = Post.objects.all()
     paginator = Paginator(post_list, 6)
@@ -38,7 +49,8 @@ def blog(request):
     context = {
         'queryset': paginated_queryset,
         'most_recent': most_recent,
-        'page_request_var': page_request_var
+        'page_request_var': page_request_var,
+        'category_count': category_count
     }
     return render(request, "blog.html", context)
 
